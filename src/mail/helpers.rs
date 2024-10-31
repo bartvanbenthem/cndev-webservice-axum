0d2673abde483f7f88fb188d4cc75fd909b8c126
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-10-31 20:14:18 +0100

    update tls

--- src/mail/helpers.rs
+++ src/mail/helpers.rs
@@ -7,16 +7,16 @@ use crate::mail::MailConfiguration;
 pub fn build_tls_parameters(config: &MailConfiguration) -> Result<TlsParameters, Box<dyn Error>> {
     // Read the certificate file
     let pem_cert = fs::read(&config.tls_cert)
-        .map_err(|e| Box::<dyn Error>::from(format!("Failed to read certificate file: {}", e)))?;
+        .map_err(|e| format!("Failed to read certificate file: {}", e))?;
     
     // Convert PEM to rustls Certificate
     let cert = Certificate::from_pem(&pem_cert)
         .map_err(|e| format!("Failed to build certificate from PEM: {}", e))?;
 
-    let tls = TlsParameters::builder("smtp.server.com".to_owned())
+    let tls = TlsParameters::builder(config.tls_domain.to_owned())
         .add_root_certificate(cert)
         .build()
-        .map_err(|e| Box::<dyn Error>::from(format!("Failed to build TlsParameters: {}", e)))?;
+        .map_err(|e| format!("Failed to build TlsParameters: {}", e))?;
 
     Ok(tls)
 }
